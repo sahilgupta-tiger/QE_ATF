965187b8bc43ae00af77b5feb3109d03319d997c
sahil.gupta <sahil.gupta@tigeranalytics.com>
2023-12-06 16:07:31 +0530

    changes for windows OS

--- scripts/startcountdemo.sh
+++ scripts/startcountdemo.sh
@@ -1,1 +1,2 @@
-bash testingstart.sh /app/test/testprotocol/testprotocol.xlsx count $1
+#bash testingstart.sh /app/test/testprotocol/testprotocol.xlsx count $1
+bash testingstart.sh /test/testprotocol/testprotocol.xlsx count $1
