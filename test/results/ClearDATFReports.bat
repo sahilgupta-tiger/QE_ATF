f328ffa0ea329af617dedd1a62a2c4406fd3291b
sahil.gupta <sahil.gupta@tigeranalytics.com>
2024-02-08 14:21:43 +0530

    merged into main branch

--- test/results/ClearDATFReports.bat
+++ test/results/ClearDATFReports.bat
@@ -1,6 +1,6 @@
 @echo OFF
 title Empty Out DATF Reports
 echo ::Deleting all files from Charts folder and Protocol PDFs::
-powershell.exe "Get-ChildItem -Path 'DBTablesDemo' -File -Recurse | Remove-Item -Recurse -Force"
-powershell.exe "Get-ChildItem -Path 'DBTablesDemo' -Directory -Recurse | Remove-Item -Recurse -Confirm:$false -Force"
+powershell.exe "Get-ChildItem -Path 'DATFDemo' -File -Recurse | Remove-Item -Recurse -Force"
+powershell.exe "Get-ChildItem -Path 'DATFDemo' -Directory -Recurse | Remove-Item -Recurse -Confirm:$false -Force"
 powershell.exe "Get-ChildItem -Path 'charts' -File -Recurse | Remove-Item -Force"
