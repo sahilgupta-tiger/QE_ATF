73228284398cf91e2905b906760e433a4ee04b52
SanchitKT <161800616+SanchitKT@users.noreply.github.com>
2024-05-04 20:18:16 +0530

    Update s2ttester.py
    Updated 
    spark = SparkSession.getActiveSession()
    to 
    spark = spark.getActiveSession()
    
    to fetch from databricks spark instance

--- datf_core/src/s2ttester.py
+++ datf_core/src/s2ttester.py
@@ -927,7 +927,7 @@ class S2TTester:
 
 
 if __name__ == "__main__":
-    spark = SparkSession.getActiveSession()
+    spark = spark.getActiveSession()
     log_info(spark)
     testcasesrunlist = []
     protocol_file_path = f"{root_path}test/testprotocol/testprotocol.xlsx"
