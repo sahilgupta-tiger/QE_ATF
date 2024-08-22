21879252612a1f136da1ba93b5d3fe179b8a2a42
sahilgupta-tiger <sahilgupta-tiger>
2024-08-22 09:59:52 +0000

    executed 2 TCs via databricks

--- datf_core/test/sql/testcase27_csv_csv_bigdata_match_source_to_stage_target.sql
+++ datf_core/test/sql/testcase27_csv_csv_bigdata_match_source_to_stage_target.sql
@@ -1,3 +1,3 @@
-readdatadf=spark.read.format('delimitedfile').option('delimiter',,).option('header','true').load('/app/test/data/target/ar_properties.csv')
+readdatadf=spark.read.format('delimitedfile').option('delimiter',,).option('header','true').load('file:/Workspace/Repos/sahil.gupta@tigeranalytics.com/QE_ATF/datf_core/test/data/target/ar_properties_1.csv')
 readdatadf.createOrReplaceTempView('dataview')
-spark.sql("SELECT ad_type, bathrooms, bedrooms, created_on, currency, end_date, id, l1, l2, l3, l4, l5, l6, lat, lon, price, price_period, property_type, rooms, surface_covered, surface_total, title FROM dataview tgt ")
+spark.sql("SELECT id, ad_type, end_date, created_on, lat, lon, l1, l2, l3, l4, l5, l6, rooms, bedrooms, bathrooms, surface_total, surface_covered, price, currency, price_period, title, property_type FROM dataview tgt ")
