41ef5bf5839095a59cfe7e3a23292efdd252a485
sahil.gupta <sahil.gupta@tigeranalytics.com>
2024-08-27 16:23:27 +0530

    quick updates

--- datf_core/src/website/pages/1_🧰_S2T_SQL_Generation.py
+++ datf_core/src/website/pages/1_🧰_S2T_SQL_Generation.py
@@ -1,8 +1,10 @@
 from datf_core.src.website.setpaths import *
 import streamlit as st
+import pandas as pd
 from os import listdir
 from os.path import isfile, join
 import json
+import pyodbc
 from langchain_core.messages import HumanMessage
 from langchain_openai import AzureChatOpenAI
 from datf_core.src.testconfig import *
@@ -13,6 +15,8 @@ os.environ["AZURE_OPENAI_API_KEY"] = decryptcredential(openai_json['apikey'])
 os.environ["AZURE_OPENAI_ENDPOINT"] = openai_json['endpoint']
 openai_api_version = openai_json['apiversion']
 azure_deployment = openai_json['deployment']
+conn_str = f"DRIVER={{SQL Server}};SERVER={'server'};DATABASE={'database'};UID={'username'};PWD={'password'}"
+conn = """pyodbc.connect(conn_str)"""
 
 
 def get_queries_from_ai(prompt):
@@ -42,6 +46,13 @@ def read_test_configs():
     st.write("You selected: ", option)
 
 
+# Function to fetch column names
+def get_column_names(connection, table_name):
+    query = f"SELECT * FROM {table_name} WHERE 1=0"
+    df = pd.read_sql(query, connection)
+    return df.columns.tolist()
+
+
 def s2t_sql_generation():
 
     st.set_page_config(
@@ -49,6 +60,42 @@ def s2t_sql_generation():
     )
     st.title("Source to Target SQL Generator")
     read_test_configs()
+    if st.button("Test Connection with Source & Target"):
+        pass
+    with (st.expander("Click to Generate Source SQL Query")):
+        src_table = ""
+        source_columns = get_column_names(conn, src_table)
+        source_column_selection = st.multiselect("Select Source Columns", source_columns)
+        prompt = st.text_area("Enter your prompt for SQL generation")
+        if st.button("Generate Source SQL"):
+            final_prompt = f"Generate a SQL query with following requirements-{prompt}"
+            final_prompt += f"\nAnd use these Columns names as reference: {', '.join(source_column_selection)}"
+            response = get_queries_from_ai(final_prompt)
+            sql_query = response.strip()
+            st.code(sql_query, language='sql')
+            # Execute the SQL on the source or target connection
+            if st.button("Run SQL on Source"):
+                sql_query += " LIMIT 5"
+                source_result = pd.read_sql(sql_query, conn)
+                st.write("Query Results from Source:")
+                st.dataframe(source_result)
+
+    with (st.expander("Click to Generate Target SQL Query")):
+        target_columns = get_column_names(conn, src_table)
+        target_column_selection = st.multiselect("Select Target Columns", target_columns)
+        tgt_prompt = st.text_area("Enter your prompt for SQL generation")
+        if st.button("Generate Target SQL"):
+            final_tgt_prompt = f"Generate a SQL query with following requirements-{tgt_prompt}"
+            final_tgt_prompt += f"\nAnd use these Columns names as reference: {', '.join(target_column_selection)}"
+            tgt_response = get_queries_from_ai(final_tgt_prompt)
+            tgt_sql_query = tgt_response.strip()
+            st.code(tgt_sql_query, language='sql')
+            tgt_sql_query += " LIMIT 5"
+            # Execute the SQL on the source or target connection
+            if st.button("Run SQL on Source"):
+                target_result = pd.read_sql(sql_query, conn)
+                st.write("Query Results from Target:")
+                st.dataframe(target_result)
 
 
 if __name__ == "__main__":
--- datf_core/src/website/pages/2_🏢_Bulk_SQL_Generation.py
+++ datf_core/src/website/pages/2_🏢_Bulk_SQL_Generation.py
@@ -4,6 +4,8 @@ import json
 from langchain_core.messages import HumanMessage
 from langchain_openai import AzureChatOpenAI
 from datf_core.src.testconfig import *
+from os import listdir
+from os.path import isfile, join
 
 
 openai_json = json.load(open(f"{core_path}/test/connections/azure_open_ai_connection.json"))
@@ -26,12 +28,45 @@ def get_queries_from_ai(prompt):
     return output_value.content
 
 
+def save_uploadedfile(uploadedfile, filepath):
+    with open(os.path.join(filepath, uploadedfile.name), "wb") as f:
+        f.write(uploadedfile.getbuffer())
+    return st.success(f"Saved File: {uploadedfile.name} to '{filepath}' in framework!")
+
+
+def file_upload_all(uploaded_file, file_type, convention):
+
+    if uploaded_file is not None:
+        name_present = False
+        tc_path = f"{core_path}/test/{file_type}"
+        onlyfiles = [f for f in listdir(tc_path) if isfile(join(tc_path, f))]
+        for loop in onlyfiles:
+            if uploaded_file.name == loop:
+                name_present = True
+                break
+
+        if name_present:
+            st.error("Filename is already in use. Please rename and reupload.")
+            return False
+        elif not uploaded_file.name.startswith(convention):
+            st.error(f"Filename must start with '{convention}'. Please rename and reupload.")
+            return False
+        else:
+            save_uploadedfile(uploaded_file, tc_path)
+            return True
+
+
 def bulk_generation():
 
     st.set_page_config(
         page_title="Bulk Generator"
     )
     st.title("Bulk Test Configs SQL Generator")
+
+    st.divider()
+    testcase_file = st.file_uploader("Template Excel file",
+                                     type='xlsx', accept_multiple_files=False)
+    upl_bulk = file_upload_all(testcase_file, 'sqlbulk', 'bulk_')
 
 
 if __name__ == "__main__":
