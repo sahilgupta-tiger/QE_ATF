a1293503b0d83f82345fad2b4bc654214673eebf
rajatyadav <>
2024-09-16 21:22:30 +0530

    Creating python files azure-adls data read for different data formats.

---
+++ datf_core/src/atf/common/atf_dc_read_adls_avrodata.py
@@ -0,0 +1,70 @@
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from atf.common.atf_common_functions import log_info,debugexit,readconnectionconfig,set_azure_connection_config
+from constants import *
+
+from datf_core.src.constants import root_path
+
+
+def read_adls_avrodata(tc_datasource_config, spark):
+    log_info("Reading the avro File")
+    connectionname = tc_datasource_config['connectionname']
+    connectiontype = tc_datasource_config['connectiontype']
+    resourceformat = tc_datasource_config['format']
+
+    # Reading Adls Connection Configuration
+    connectionconfig = readconnectionconfig(connectionname)
+
+    # Set Adls Connection Configuration
+    storage_account, container_name = set_azure_connection_config(connectionconfig, spark)
+    avrofilepath = tc_datasource_config['path']  # Relative path within the container
+
+    # Reading parquet from ADLS Stoarge Container
+    if 'Volumes' in avrofilepath:
+        log_info("Reading json file from ADLS via Databricks Volumes")
+        print("Reading json file from ADLS via Databricks Volumes")
+        print('Volumes File Path :', avrofilepath)
+    else:
+        log_info("Reading json file from ADLS via ABFSS protocol")
+        print("Reading json file from ADLS via ABFSS protocol")
+        avrofilepath = f"abfss://{container_name}@{storage_account}.dfs.core.windows.net/{avrofilepath}"
+        print('ADLS File Path :', avrofilepath)
+
+    if tc_datasource_config['testquerygenerationmode'] == 'Auto':
+        resourcename = tc_datasource_config['name']
+        datafilter = tc_datasource_config['filter']
+        # avrofilepath = tc_datasource_config['path']
+        excludecolumns = tc_datasource_config['excludecolumns']
+        excludecolumns = str(excludecolumns)
+        exclude_cols = excludecolumns.split(',')
+        datafilter = str(datafilter)
+        readavroschema = spark.read.format("avro").load(avrofilepath).schema
+        readavrodata = spark.read.format("avro").schema(readavroschema).load(avrofilepath)
+        df = preproc_unnestfields(readavrodata)
+        df.createOrReplaceTempView(resourcename + "_avroview")
+        columns = df.columns
+        columnlist = list(set(columns) - set(exclude_cols))
+        columnlist.sort()
+        columnlist = ','.join(columnlist)
+        query_avro = "SELECT " + columnlist + " FROM " + resourcename + "_avroview"
+        if len(datafilter) >= 5:
+            query_avro = query_avro + " WHERE " + datafilter
+        df_data = spark.sql(query_avro)
+    elif tc_datasource_config['testquerygenerationmode'] == 'Manual':
+        querypath = root_path + tc_datasource_config['querypath']
+        f = open(querypath, "r")
+        query = f.read().splitlines()
+        query = ' '.join(query)
+        print(query)
+        df = spark.read.format("avro").load(avrofilepath)
+        df.printSchema()
+        print(tc_datasource_config['aliasname'])
+        df.createOrReplaceTempView(tc_datasource_config['aliasname'])
+        df_data = spark.sql(query)
+
+    '''col_names = df_avrodata.columns
+    for i in col_names:
+      df_avrodata=df_avrodata.withColumnRenamed(i,i.lower())
+    '''
+    log_info("Returning the DataFrame from read_avrodata Function")
+    return df_data, query
--- datf_core/src/atf/common/atf_dc_read_adls_delimiteddata.py
+++ datf_core/src/atf/common/atf_dc_read_adls_delimiteddata.py
@@ -1,27 +1,33 @@
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from pyspark import StorageLevel
-from atf.common.atf_common_functions import log_info
+from atf.common.atf_common_functions import log_info,debugexit,readconnectionconfig,set_azure_connection_config
 from constants import *
 
 def read_adls_delimiteddata(tc_datasource_config,spark):
-  log_info("Reading delimited File from ADLS storage")
+  log_info("Reading delimited file from ADLS storage")
   resourcename = tc_datasource_config['aliasname']
   comparetype = tc_datasource_config['testquerygenerationmode']
   connectionname = tc_datasource_config['connectionname']
-  # connectionconfig = readconnectionconfig(connectionname)
-  # storage_account = connectionconfig['STORAGE_ACCOUNT_NAME']
-  # container_name = connectionconfig['CONTAINER_NAME']  # Assuming you have this in your config
-  csv_path = tc_datasource_config['path']  # Relative path within the container
-  # sas_token = connectionconfig['SAS_TOKEN']  # Optional SAS token
 
-  # # Set the configuration using SAS Token
-  # spark.conf.set(f"fs.azure.account.auth.type.{storage_account}.dfs.core.windows.net", "SAS")
-  # spark.conf.set(f"fs.azure.sas.token.provider.type.{storage_account}.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.sas.FixedSASTokenProvider")
-  # spark.conf.set(f"fs.azure.sas.fixed.token.{storage_account}.dfs.core.windows.net", sas_token)
-  
-  log_info("Reading delimited File  from ADLS Volume")
-  connectionname =tc_datasource_config['connectionname']
+  # Reading Adls Connection Configuration
+  connectionconfig = readconnectionconfig(connectionname)
+
+  # Set Adls Connection Configuration
+  storage_account, container_name = set_azure_connection_config(connectionconfig, spark)
+  delimited_path = tc_datasource_config['path']  # Relative path within the container
+
+  # Reading parquet from ADLS Stoarge Container
+  if 'Volumes' in delimited_path:
+    log_info("Reading delimited file from ADLS via Databricks Volumes")
+    print("Reading delimited file from ADLS via Databricks Volumes")
+    print('Volumes File Path :', delimited_path)
+  else:
+    log_info("Reading parquet file from ADLS via ABFSS protocol")
+    print("Reading parquet file from ADLS via ABFSS protocol")
+    delimited_path = f"abfss://{container_name}@{storage_account}.dfs.core.windows.net/{delimited_path}"
+    print('ADLS File Path :', delimited_path)
+
   connectiontype =tc_datasource_config['connectiontype']
   resourceformat =tc_datasource_config['format']
   delimiter =tc_datasource_config['delimiter']
@@ -29,16 +35,11 @@ def read_adls_delimiteddata(tc_datasource_config,spark):
   if tc_datasource_config['testquerygenerationmode'] == 'Auto':
     resourcename = tc_datasource_config['name']
     datafilter = tc_datasource_config['filter']
-    #mount_path = 'file:'+root_path+tc_datasource_config['path']
-    #filepath = get_mount_path(mount_path)
-    #filepath = f"abfss://{container_name}@{storage_account}.dfs.core.windows.net/{delta_path}"
-    print("CSV Path in Azure Storage : ",csv_path)
-    
     excludecolumns =tc_datasource_config['excludecolumns']
     excludecolumns = str(excludecolumns)
     exclude_cols = excludecolumns.split(',')
     datafilter = str(datafilter)
-    df = spark.read.option("delimiter", delimiter).csv(csv_path, header = True)
+    df = spark.read.option("delimiter", delimiter).csv(delimited_path, header = True)
     df.printSchema()
     df.createOrReplaceTempView(resourcename + "_csvview")
     columns = df.columns
@@ -48,17 +49,14 @@ def read_adls_delimiteddata(tc_datasource_config,spark):
     query_csv = "SELECT " + columnlist + " FROM " + resourcename + "_csvview"
     if len(datafilter) >=5:
       query_csv = query_csv + " WHERE " + datafilter
-    df_data = spark.sql(query_csv)        
+    df_data = spark.sql(query_csv)
+
   elif tc_datasource_config['testquerygenerationmode'] == 'Manual':
     querypath = root_path+tc_datasource_config['querypath']
     f = open(querypath,"r")
     query= f.read().splitlines()
     query=' '.join(query)
-    # csv_path = 'file:'+root_path+tc_datasource_config['path']
-    # csv_path = f"abfss://{container_name}@{storage_account}.dfs.core.windows.net/{delta_path}"
-    print("CSV Path in Azure Storage : ",csv_path)
-    
-    df=spark.read.option("delimiter", tc_datasource_config['delimiter']).schema(tc_datasource_config['schemastruct']).csv(csv_path, header = True)
+    df=spark.read.option("delimiter", delimiter).schema(tc_datasource_config['schemastruct']).csv(delimited_path, header = True)
     df.printSchema()
     print(tc_datasource_config['aliasname'])
     df.createOrReplaceTempView(tc_datasource_config['aliasname'])
