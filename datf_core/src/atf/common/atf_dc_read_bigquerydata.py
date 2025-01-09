7845637074260bbb91dc35e69abdf4abd304c57d
venkatesh.thiyag <venkatesh.thiyag@tigeranalytics.com>
2025-01-09 14:21:06 +0530

    Update atf_dc_read_bigquerydata.py
    Removed the notebook like Magic commands

--- datf_core/src/atf/common/atf_dc_read_bigquerydata.py
+++ datf_core/src/atf/common/atf_dc_read_bigquerydata.py
@@ -1,19 +1,10 @@
-# Databricks notebook source
-# DBTITLE 1, Import Required Libraries
+
 from pyspark.sql.functions import * 
 from pyspark.sql.types import *
 from atf.common.atf_common_functions import log_info, readconnectionconfig
 import os
 
 
-# COMMAND ----------
-
-# DBTITLE 1,Load Common Functions notebook
-# MAGIC %run ./atf_common_functions
-
-# COMMAND ----------
-
-# DBTITLE 1,Function to read Big Query data
 def read_bigquerydata(tc_datasource_config, spark):
   log_info("Reading from Big Query Table")
 
