036b728fe7b68707c4c02b2ee08209b289132f41
venkatesh.thiyag <venkatesh.thiyag@tigeranalytics.com>
2025-01-09 14:42:48 +0530

    Update atf_sc_read_jsonschema.py
    Removed the notebook commands

--- datf_core/src/atf/common/atf_sc_read_jsonschema.py
+++ datf_core/src/atf/common/atf_sc_read_jsonschema.py
@@ -1,4 +1,4 @@
-# Databricks notebook source
+
 def read_jsonschema(dict_connection, comparetype):
   connectionname = dict_connection['connectionname']
   s3bucket = get_connection_config(connectionname)['BUCKETNAME']
