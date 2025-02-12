0c16acd7dd9a9500c4d9a4f07195dc74f4daeae9
venkatesh-thiyag <venkatesh-thiyag>
2025-02-12 13:58:46 +0000

    Modified the code

--- datf_core/src/atf/common/atf_great_expectation_dq.py
+++ datf_core/src/atf/common/atf_great_expectation_dq.py
@@ -144,7 +144,7 @@ def ge_test_execution(pdfobj,batch,rows):
             else:
                 Result = "Failed"
             pdfobj.write_text(f"{i+1}.Testcase_{columnname}_{Dqtype}_Validation", 'section heading')
-            dict_result = {"Test Status": Result, "Column Name": columnname, "DQ Validation": Dqtype, "Expected Value": expected,  "Element Count": count, "Unexpected Count": unexpected_count, "Additional Values Other Than Expected": observed_value}
+            dict_result = {"Test Status": Result, "Column Name": columnname, "DQ Validation": Dqtype, "Expected Value": expected, "Actual Value": observed_value}
         if check == "ColumnCount":
             status = validation_results.success
             expectation  = gx.expectations.ExpectTableColumnCountToEqual(value=value)
