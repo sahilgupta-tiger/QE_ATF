85e814de2b7b8f1dc956466328640576a7569c9b
venkatesh-thiyag <venkatesh-thiyag>
2025-02-12 10:38:07 +0000

    modified great expectation

--- datf_core/src/atf/common/atf_great_expectation_dq.py
+++ datf_core/src/atf/common/atf_great_expectation_dq.py
@@ -1,42 +1,35 @@
 import great_expectations as gx
-import os
-import pandas as pd
-import json
-from atf.common.atf_cls_pdfformatting import generatePDF
-from atf.common.atf_pdf_constants import *
-
-def dq_analyser(tcname,dejavu_path, rows, df):
-    pdfobj = generatePDF(dejavu_path)
-    pdfobj.write_text("Data Quality Analysis Report", 'report header')
-    resultfolder =  dejavu_path + "/test/results/data_quality/"+tcname
-    if not os.path.exists(resultfolder):
-        log_info(f"Creating directory - {resultfolder}")
-        os.mkdir(resultfolder)
-    else:
-        log_info(f"The directory - `{resultfolder}` already exists.")
-
-    print(rows)
-    for item in rows:
-        if item["DQ Check"] == "Regexp" or item["DQ Check"] == "DistinctSet":
-            item["Value"] = json.loads(item["Value"])
+def ge_test_initalization(df):
+    df = df
+    # Retrieve your Data Context
     context = gx.get_context()
+
     # Define the Data Source name
     data_source_name = "my_data_source"
+
     # Add the Data Source to the Data Context
     data_source = context.data_sources.add_spark(name=data_source_name)
     print(data_source)
     data_asset_name = "my_dataframe_data_asset"
+
     # Add a Data Asset to the Data Source
     data_asset = data_source.add_dataframe_asset(name=data_asset_name)
     print(data_asset)
     batch_definition_name = "my_batch_definition"
+
     # Add a Batch Definition to the Data Asset
     batch_definition = data_asset.add_batch_definition_whole_dataframe(
         batch_definition_name
     )
     batch_parameters = {"dataframe": df}
-    batch = batch_definition.get_batch(batch_parameters=batch_parameters)
+    batch = batch_definition.get_batch(batch_parameters=batch_parameters)   
+    return batch
+
+def ge_test_execution(batch,rows):
+    batch = batch
+    rows = rows
     for i,row in enumerate(rows):
+
         column = row["Column"]
         value = row["Value"]
         check = row["DQ Check"]
@@ -54,6 +47,12 @@ def dq_analyser(tcname,dejavu_path, rows, df):
             unexpected_count = validation_results.result["unexpected_count"]
             observed_value = validation_results.result["partial_unexpected_list"]
             columnname = validation_results.expectation_config.kwargs["column"]
+            if status:
+                Result = "Passed"
+            else:
+                Result = "Failed"
+            pdfobj.write_text(f"{i+1}.Testcase_{columnname}_{Dqtype}_{dqtype_valid}", 'section heading')
+            dict_result = {"Test_Status": Result, "column Name": columnname, "DQ Check": Dqtype, "Expected Length": expected,  "Element Count": count, "Unexpected Count": unexpected_count, "Sample Mismatches": observed_value}
         if check == "Length" and check_type == "Between":
             expectation  = gx.expectations.ExpectColumnValueLengthsToBeBetween(column=column, min_value = value.split("-")[0], max_value = value.split("-")[1])
             validation_results = batch.validate(expectation)
@@ -66,6 +65,12 @@ def dq_analyser(tcname,dejavu_path, rows, df):
             unexpected_count = validation_results.result["unexpected_count"]
             observed_value = validation_results.result["partial_unexpected_list"]
             columnname = validation_results.expectation_config.kwargs["column"]
+            if status:
+                Result = "Passed"
+            else:
+                Result = "Failed"
+            pdfobj.write_text(f"{i+1}.Testcase_{columnname}_{Dqtype}_{dqtype_valid}", 'section heading')
+            dict_result = {"Test_Status": Result, "column Name": columnname, "DQ Check": Dqtype, "Expected Length Range": expected,  "Element Count": count, "Unexpected Count": unexpected_count, "Sample Mismatches": observed_value}
         if check == "Null":
             expectation  = gx.expectations.ExpectColumnValuesToBeNull(column=column)
             validation_results = batch.validate(expectation)
@@ -78,6 +83,12 @@ def dq_analyser(tcname,dejavu_path, rows, df):
             unexpected_count = validation_results.result["unexpected_count"]
             observed_value = validation_results.result["partial_unexpected_list"]
             columnname = validation_results.expectation_config.kwargs["column"]
+            if status:
+                Result = "Passed"
+            else:
+                Result = "Failed"
+            pdfobj.write_text(f"{i+1}.Testcase_{columnname}_{Dqtype}", 'section heading')
+            dict_result = {"Test_Status": Result, "column Name": columnname, "DQ Check": Dqtype, "Expected Value": expected,  "Element Count": count, "Unexpected Count": unexpected_count, "Sample Mismatches": observed_value}
         if check == "NotBeNull":
             expectation  = gx.expectations.ExpectColumnValuesToNotBeNull(column=column)
             validation_results = batch.validate(expectation)
@@ -90,6 +101,12 @@ def dq_analyser(tcname,dejavu_path, rows, df):
             unexpected_count = validation_results.result["unexpected_count"]
             observed_value = validation_results.result["partial_unexpected_list"]
             columnname = validation_results.expectation_config.kwargs["column"]
+            if status:
+                Result = "Passed"
+            else:
+                Result = "Failed"
+            pdfobj.write_text(f"{i+1}.Testcase_{columnname}_{Dqtype}", 'section heading')
+            dict_result = {"Test_Status": Result, "column Name": columnname, "DQ Check": Dqtype, "Expected Value": expected,  "Element Count": count, "Unexpected Count": unexpected_count, "Sample Mismatches": observed_value}
         if check == "Unique":
             expectation  = gx.expectations.ExpectColumnValuesToBeUnique(column=column)
             validation_results = batch.validate(expectation)
@@ -102,9 +119,16 @@ def dq_analyser(tcname,dejavu_path, rows, df):
             unexpected_count = validation_results.result["unexpected_count"]
             observed_value = validation_results.result["partial_unexpected_list"]
             columnname = validation_results.expectation_config.kwargs["column"]
+            if status:
+                Result = "Passed"
+            else:
+                Result = "Failed"
+            pdfobj.write_text(f"{i+1}.Testcase_{columnname}_{Dqtype}", 'section heading')
+            dict_result = {"Test_Status": Result, "column Name": columnname, "DQ Check": Dqtype, "Expected Value": expected,  "Element Count": count, "Unexpected Count": unexpected_count, "Sample Mismatches": observed_value}
         if check == "DistinctSet":
             expectation  = gx.expectations.ExpectColumnDistinctValuesToBeInSet(column=column, value_set=value)
-            validation_results = batch.validate(expectation)    
+            validation_results = batch.validate(expectation)
+            
             #print(validation_results)
             Dqtype = check
             dqtype_valid = check_type
@@ -114,6 +138,12 @@ def dq_analyser(tcname,dejavu_path, rows, df):
             status = validation_results.success
             observed_value = validation_results.result["observed_value"][:50]
             columnname = validation_results.expectation_config.kwargs["column"]
+            if status:
+                Result = "Passed"
+            else:
+                Result = "Failed"
+            pdfobj.write_text(f"{i+1}.Testcase_{columnname}_{Dqtype}", 'section heading')
+            dict_result = {"Test_Status": Result, "column Name": columnname, "DQ Check": Dqtype, "Expected Value": expected,  "Element Count": count, "Unexpected Count": unexpected_count, "Sample Mismatches": observed_value}
         if check == "ColumnCount":
             status = validation_results.success
             expectation  = gx.expectations.ExpectTableColumnCountToEqual(value=value)
@@ -126,6 +156,12 @@ def dq_analyser(tcname,dejavu_path, rows, df):
             unexpected_count = None
             observed_value = validation_results.result["observed_value"]
             columnname = column
+            if status:
+                Result = "Passed"
+            else:
+                Result = "Failed"
+            pdfobj.write_text(f"{i+1}.Testcase_{Dqtype}", 'section heading')
+            dict_result = {"Test_Status": Result, "DQ Check": Dqtype, "Expected Count": expected,  "Actual Count": observed_value}
         if check == "Regexp":
             expectation = gx.expectations.ExpectColumnValuesToMatchRegexList(column=column,regex_list=value,match_on="any")
             validation_results = batch.validate(expectation)
@@ -138,16 +174,10 @@ def dq_analyser(tcname,dejavu_path, rows, df):
             unexpected_count = validation_results.result["unexpected_count"]
             observed_value = validation_results.result["partial_unexpected_list"]
             columnname = validation_results.expectation_config.kwargs["column"]
-        if status:
-            Result = "Passed"
-        else:
-            Result = "Failed"
-        pdfobj.write_text(f"{i+1}.Testcase_{columnname}_{Dqtype}", 'section heading')
-        dict_result = {"Test_Status": Result, "column Name": columnname, "DQ Check": Dqtype, "Expected Value": expected,  "Element Count": count, "Unexpected Count": unexpected_count, "Observed Value": observed_value}
+            if status:
+                Result = "Passed"
+            else:
+                Result = "Failed"
+            pdfobj.write_text(f"{i+1}.Testcase_{columnname}_{Dqtype}", 'section heading')
+            dict_result = {"Test_Status": Result, "column Name": columnname, "DQ Check": Dqtype, "Expected Pattern": expected,  "Element Count": count, "Unexpected Count": unexpected_count, "Sample Mismatches": observed_value}
         pdfobj.create_table_summary(dict_result)
-        pdfobj.write_text("Refer below for more information", 'section heading')
-        pdfobj.create_table_summary(validation_results)
-    timenow = datetime.now(utctimezone)
-    created_time = str(timenow.astimezone(utctimezone).strftime("%d_%b_%Y_%H_%M_%S_%Z"))
-    resultpath = resultfolder + "/data_quality_analysis_report_"+created_time+".pdf"
-    pdfobj.pdf.output(resultpath, 'F')
