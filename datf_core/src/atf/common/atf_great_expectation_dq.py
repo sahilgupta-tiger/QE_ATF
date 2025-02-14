79504477db9c4f889b6b065ddaa4000ab0296a96
venkatesh-thiyag <venkatesh-thiyag>
2025-02-14 07:26:07 +0000

    Converted pandas df to spark df

--- datf_core/src/atf/common/atf_great_expectation_dq.py
+++ datf_core/src/atf/common/atf_great_expectation_dq.py
@@ -346,6 +346,7 @@ def generate_protocol_summary_report(df_testsuite_summary,  testsuite,protocol_r
         testheader = "2." + str(sno) + ". " + test_result + " Testcases"
         pdfobj_summary.write_text(testheader, 'section heading')
         if (df_testsuite_summary is not None):
+            df_testsuite_summary = spark.createDataFrame(df_testsuite_summary)
             df_testsuite_summary_temp = df_testsuite_summary.select(
                 '*').filter(col('Test Result') == test_result)
             if (df_testsuite_summary_temp.count() == 0):
