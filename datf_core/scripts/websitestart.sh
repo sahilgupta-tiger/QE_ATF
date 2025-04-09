14c80ada20860399149e5ca19bb891409e5d0d8f
sahil.gupta <sahil.gupta@tigeranalytics.com>
2025-04-09 20:53:22 +0530

    updated echo command

--- datf_core/scripts/websitestart.sh
+++ datf_core/scripts/websitestart.sh
@@ -1,3 +1,3 @@
-echo Tiger-DATF Web Portal Started
+echo "Tiger-DATF Web Portal Started"
 streamlit run 'datf_app/datf_portal.py'
-echo Tiger-DATF Web Portal Completed
+echo "Tiger-DATF Web Portal Completed"
