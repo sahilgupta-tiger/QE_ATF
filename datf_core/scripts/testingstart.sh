dae13c164e0c61313fbe7409f1b314bf8a813c18
sahil.gupta <sahil.gupta@tigeranalytics.com>
2025-04-09 21:07:38 +0530

    removed echo

--- datf_core/scripts/testingstart.sh
+++ datf_core/scripts/testingstart.sh
@@ -1,3 +1,1 @@
-echo "Tiger-DATF Execution Started"
 /opt/spark/bin/spark-submit --driver-memory 6g --driver-class-path $(echo datf_core/test/jdbcdriver/*.jar | tr ' ' ',') --jars $(echo datf_core/test/jdbcdriver/*.jar | tr ' ' ',') datf_core/src/s2ttester.py $1 $2
-echo "Tiger-DATF Execution Completed"
--- datf_core/scripts/websitestart.sh
+++ datf_core/scripts/websitestart.sh
@@ -1,3 +1,1 @@
-echo "Tiger-DATF Web Portal Started"
 streamlit run 'datf_app/datf_portal.py'
-echo "Tiger-DATF Web Portal Completed"
